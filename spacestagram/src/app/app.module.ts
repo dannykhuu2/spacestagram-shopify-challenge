b5db66271da5155951d2975e8db05afe998027e5
dannykhuu2 <dannykhuu2@gmail.com>
2021-09-16 16:04:28 -0400

    set up basic routing, make first components, and install angular bootstrap

--- spacestagram/src/app/app.module.ts
+++ spacestagram/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageContainerComponent } from './components/image-container/image-container.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageContainerComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
